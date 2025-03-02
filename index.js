b2dceb706756273dcf816dcbf75a94c11366cefd
Yash <yadavyashsantaji@gmail.com>
2025-03-03 01:50:56 +0530

    time

--- index.js
+++ index.js
@@ -1,32 +1,28 @@
 
+
 function showtime() {
-    const time = document.getElementById("time");
-    let date = new Date();
-    date=date.toUTCString()
-    now = date.substring(6,11)+", "+date.substring(14,17)+"| "+"09:58"+" AM"
-
-    let data="hello"
-    time.textContent =now;
-  }
-  showtime();
-
-  function showtime2() {
+  const time = document.getElementById("time");
     const time2 = document.getElementById("time2");
-    let date2 = new Date();
-    date2=date2.toUTCString()
-    now2 = date2.substring(6,11)+", "+date2.substring(14,17)+"| "+"11:59"+" AM"
+    
+    let date = new Date();
+    date=date.toString();
+    
+    let now = date.substring(8,10)+date.substring(3,7)+","+date.substring(13,15)+"| "+"09:58"+" AM";
+    let now2 = date.substring(8,10)+date.substring(3,7)+","+date.substring(13,15)+"| "+"11:59"+" AM"
+    
+    time.textContent =now;
     time2.textContent =now2;
   }
-  showtime2();
-
-
-  let p_date=new Date()
-  p_id=p_date.toISOString()
-  a_id=p_id.substring(2,4)+p_id.substring(5,7)+p_id.substring(8,10)+"958KK3RVQ"
-
+  
+  let date=new Date()
+  let month=date.getMonth().toString().padStart(2,0)
+  date=date.toString()
+  let uid=date.substring(13,15)+month+date.substring(8,10)+"958KK3RVQ"
+  
   function idgenerater(){
-        let id=document.getElementById("p_id")
-        id.textContent=a_id;
+    let id=document.getElementById("p_id")
+    id.textContent=uid;
   }
-
-  idgenerater()
+  
+  idgenerater();
+  showtime();
